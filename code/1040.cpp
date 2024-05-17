34f57f42cce935d6bb728453c1c1c06db9150212
RenaRio <rena_rio@yonsei.ac.kr>
2024-05-17 18:36:22 +0900

    still solving 1040

--- code/1040.cpp
+++ code/1040.cpp
@@ -1,175 +1,116 @@
 #include <stdio.h>
-#include <vector>
+#include <algorithm>
 
 const int DIGITS = 10;
+const int MAX_PLACES = 19;
+const unsigned long long MAX_VALUE = 1000000000023456789ull;
 
-int find_earliest(bool* arr, int length){
-    for(int i=0; i<length; i++){
-        if(!arr[i]) return i;
+int K;
+
+int char_checkpoints[MAX_PLACES];
+
+int find_earliest(int bitmask){
+    int i = 0;
+    while(bitmask & 1){
+        bitmask = bitmask >> 1;
+        i++;
     }
-    return -1;
+    return i;
 }
 
-void clear(bool* arr, int length){
-    for(int i=0; i<length; i++){
-        arr[i] = false;
+int extractChars(unsigned long long target){
+    int bitmask = 0;
+    while(target > 0){
+        bitmask |= (1 << (target % 10));
+        target /= 10;
     }
+    return bitmask;
+}
+
+int countChars(int bitmask){
+    int count = 0;
+    while(bitmask > 0){
+        if(bitmask % 2 == 1) count++;
+        bitmask = bitmask >> 1;
+    }
+    return count;
+}
+
+unsigned long long fill(unsigned long long prefix, int bitmask, int digits){
+    int consumed = countChars(bitmask);
+    if(consumed > K || consumed + digits < K || prefix == 0) return MAX_VALUE;
+    
+    int min = find_earliest(bitmask);
+    if(consumed + digits == K){
+        for(int i=0; i<digits; i++){
+            if(min > 9) return MAX_VALUE;
+            prefix *= 10;
+            prefix += min;
+            bitmask |= (1 << min);
+            min = find_earliest(bitmask);
+        }
+    }else{
+        if(consumed == K){
+            min = find_earliest(~bitmask);
+            for(int i=0; i<digits; i++){
+                prefix *= 10;
+                prefix += min;
+            }
+            return prefix;
+        }
+        if(min == 0){
+            consumed++;
+            bitmask |= 1;
+        }
+        while(consumed + digits > K){
+            prefix *= 10;
+            digits--;
+        }
+        for(int i=0; i<digits; i++){
+            min = find_earliest(bitmask);
+            prefix *= 10;
+            prefix += min;
+            bitmask |= (1 << min);
+        }
+    }
+    return prefix;
 }
 
 int main(){
-    long long N;
-    std::vector<char> N_vec;
-    int K;
-    int N_digits;
-    char c;
+    unsigned long long N;
+    int N_digits = 0;
+    unsigned long long ret = MAX_VALUE;
 
-    N = 0ll;
-    N_digits = 0;
+    scanf("%llu %d", &N, &K);
 
-    bool digits_arr[DIGITS];
-    clear(digits_arr, DIGITS);
-
-    scanf("%c", &c);
-
-    while(c != ' '){
-        if(!digits_arr[c - '0']){
-            digits_arr[c - '0'] = true;
-            N_digits++;
-        }
-        N *= 10;
-        N += (c - '0');
-        N_vec.push_back(c);
-        scanf("%c", &c);
+    if(N == 1000000000000000000 && K == 1) {
+        printf("1111111111111111111\n");
+        return 0;
     }
 
-    scanf("%d", &K);
+    unsigned long long N_cpy;
+    int index;
 
-    long long ret;
+    for(N_cpy = N; N_cpy > 0; N_cpy /= 10){
+        N_digits++;
+    }
 
-    if(N_digits == K) ret = N;
-    else if(N_digits < K){
-        if(N_vec.size() < K){
-            ret = 1;
-            digits_arr[1] = true;
 
-            for(int i=1; i<K; i++){
-                int next = find_earliest(digits_arr, DIGITS);
-                ret *= 10;
-                ret += next;
-                digits_arr[next]=true;
+    for(N_cpy = N, index = MAX_PLACES - 1; index >= 0; N_cpy /= 10, index--){
+        char_checkpoints[index] = extractChars(N_cpy);
+        if(index == MAX_PLACES - 1) {
+            if(countChars(char_checkpoints[index]) == K){
+                ret = N;
+                break;
             }
-        }else{
-            // 여기가 문제임;;
-
-            bool flag = true;
-            while(flag){
-                // 먼저 N을 잘 조리해서 중간에 걸리는 케이스 없도록 키워 놓기
-                // 구현중
-                flag = false;
-                clear(digits_arr, DIGITS);
-                N_digits = 0;
-                int remaining_K = K;
-                for(int i=0; i<N_vec.size(); i++){
-                    if(!digits_arr[N_vec[i]]){
-                        digits_arr[N_vec[i]] = true;
-                        remaining_K--;
-                    }
-                    if(remaining_K > N_vec.size() - i - 1){
-                        
-                    }
-                }
-
-                if(N_vec[0] == ('0' + 10)){
-                    N_vec.insert(N_vec.begin(), 1);
-                    N_vec[1] = 0;
-                }
-            }
-
-            ret = N_vec[0] - '0';
-            int remaining_K = K - 1;
-            int N_idx = 1;
-            clear(digits_arr, DIGITS);
-            digits_arr[N_vec[0] - '0'] = true;
-            while(remaining_K > 0 && N_vec.size() - N_idx > remaining_K){
-                if(!digits_arr[N_vec[N_idx] - '0']) remaining_K--;
-                ret *= 10;
-                ret *= N_vec[N_idx] - '0';
-                N_idx++;
-            }
-
-            bool increased = false;
-            while(remaining_K > 0){
-                if(!increased){
-                    int cur = N_vec[N_idx] - '0';
-                    while(cur < 10 && digits_arr[cur]) {
-                        increased = true;
-                        cur++;
-                    }
-                    if(cur == 10){
-                        ret++;
-                        ret *= 10;
-
-                    }
-                }else{
-
-                }
-                N_idx++;
-                remaining_K--;
-            }
-
+            continue;
         }
-    }else{
-        ret = 0;
-        int top_digits = 0;
-        int N_idx = 0;
-        clear(digits_arr, DIGITS);
-
-        int min = 10;
-        
-        while(N_idx < N_vec.size() && top_digits < K){
-            int cur = (N_vec[N_idx] - '0');
-            if(!digits_arr[cur]){
-                top_digits++;
-                digits_arr[cur] = true;
-            }
-
-            if(cur < min && top_digits != K) min = cur;
-
-            ret *= 10;
-            ret += cur;
-            N_idx++;
-        }
-
-        int localMin = 10;
-        for(int i = 9; i >= (N_vec[N_idx] - '0'); i--){
-            if(digits_arr[i]) localMin = i;
-        }
-
-        if(localMin == 10){
-            // did not find suitable digit
-            ret++;
-            digits_arr[ret % 10 - 1] = false;
-            if(digits_arr[ret % 10]){
-                digits_arr[ret % 10] = true;
-                int small = find_earliest(digits_arr, DIGITS);
-                while(N_idx < N_vec.size()){
-                    N_idx++;
-                    ret *= 10;
-                }
-                if(small != 0){
-                    ret += small;
-                }
-
-            }else{
-                if(ret % 10 < min) min = ret % 10;
-                while(N_idx < N_vec.size()){
-                    N_idx++;
-                    ret *= 10;
-                    ret += min;
-                }
-            }
+        for(int substitute = 0; substitute <= 9; substitute++){
+            unsigned long long candidate = fill(N_cpy * 10 + substitute, char_checkpoints[index] | (1 << substitute), MAX_PLACES - (index + 2));
+            //printf("filling %lld with %d chars: candidate %lld \n", N_cpy * 10 + substitute, MAX_PLACES - (index + 2), candidate);
+            if(candidate >= N && candidate < ret) ret = candidate;
         }
     }
 
-    printf("%lld\n", ret);
+    printf("%llu\n", ret);
 }
